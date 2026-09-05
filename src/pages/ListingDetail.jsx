9f7c4a201435fd4880838bd359c53e048f3cecc7
Misha1986999 <zxmishaxz86@mail.ru>
2026-09-06 01:34:41 +1000

    зам

--- src/pages/ListingDetail.jsx
+++ src/pages/ListingDetail.jsx
@@ -64,7 +64,7 @@ export default function ListingDetail() {
         <div className="border-t border-ink/10 pt-4">
           {showPhone ? (
             
-              href={'tel:' + listing.phone}
+             <a href={'tel:' + listing.phone}
               className="inline-block bg-bay text-white px-5 py-3 rounded-md font-medium"
             >
               {listing.phone}
